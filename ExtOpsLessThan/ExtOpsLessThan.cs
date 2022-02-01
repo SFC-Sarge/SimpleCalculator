7e6b9aa592eb759dd595b53597caed6e553bd4e2
SFC-Sarge <danny.mcnaught@dannymcnaught.com>
2022-01-31 22:39:01 -0500

    Added LessThan and LessThanEqualTo

---
+++ ExtOpsLessThan/ExtOpsLessThan.cs
@@ -0,0 +1,36 @@
+﻿using SimpleCalculator;
+
+using System.ComponentModel.Composition;
+
+namespace ExtOpsLessThan
+{
+    public class ExtOpsLessThan
+    {
+        [Export(typeof(IOperation))]
+        [ExportMetadata("Symbol", "<")]
+        public class LessThan : IOperation
+        {
+
+            bool IOperation.Equality(bool left, bool right)
+            {
+                throw new NotImplementedException();
+            }
+
+            bool IOperation.Equality(int left, int right)
+            {
+                return (left < right);
+            }
+
+            bool IOperation.Equality(object left, object right)
+            {
+                throw new NotImplementedException();
+            }
+
+            int IOperation.Operate(int left, int right)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
---
+++ ExtOpsLessThanEqualTo/ExtOpsLessThanEqualTo.cs
@@ -0,0 +1,36 @@
+﻿using SimpleCalculator;
+
+using System.ComponentModel.Composition;
+
+namespace ExtOpsLessThanEqualTo
+{
+    public class ExtOpsLessThanEqualTo
+    {
+        [Export(typeof(IOperation))]
+        [ExportMetadata("Symbol", "<=")]
+        public class LessThanEqualTo : IOperation
+        {
+
+            bool IOperation.Equality(bool left, bool right)
+            {
+                throw new NotImplementedException();
+            }
+
+            bool IOperation.Equality(int left, int right)
+            {
+                return (left <= right);
+            }
+
+            bool IOperation.Equality(object left, object right)
+            {
+                throw new NotImplementedException();
+            }
+
+            int IOperation.Operate(int left, int right)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
